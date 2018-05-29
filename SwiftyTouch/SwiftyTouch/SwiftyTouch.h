55458124a10f9423e13647ad6d5ce78122aff8ab
Xavier Leporcher <x.leporcher@gmail.com>
2018-05-29 17:48:18 +0200

    Add Workspace

---
+++ SwiftyTouch/SwiftyTouch/SwiftyTouch.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyTouch.h
+//  SwiftyTouch
+//
+//  Created by Xavier Leporcher on 29/05/2018.
+//  Copyright © 2018 LEPandaRouX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyTouch.
+FOUNDATION_EXPORT double SwiftyTouchVersionNumber;
+
+//! Project version string for SwiftyTouch.
+FOUNDATION_EXPORT const unsigned char SwiftyTouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTouch/PublicHeader.h>
+
+
