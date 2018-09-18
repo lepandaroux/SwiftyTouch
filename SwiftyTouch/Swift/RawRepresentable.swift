747acab23dc37277eac67fe0267032dc40ab21a7
Xavier Leporcher <x.leporcher@gmail.com>
2018-09-18 15:28:09 +0200

    Highlights Swift 4.2’s CaseIterable protocol

--- SwiftyTouch/Swift/RawRepresentable.swift
+++ SwiftyTouch/Swift/RawRepresentable.swift
@@ -8,6 +8,7 @@
 
 public extension RawRepresentable where Self: Hashable {
     
+    @available(swift, obsoleted: 4.2, message: "Swift 4.2 introduces the `CaseIterable` protocol; prefer using the last.")
     public static var values: [Self] {
         var values = [Self]()
         var raw = 0
@@ -20,6 +21,7 @@ public extension RawRepresentable where Self: Hashable {
         return values
     }
     
+    @available(swift, obsoleted: 4.2, message: "Swift 4.2 introduces the `CaseIterable` protocol; prefer using the last.")
     public static var rawValues: [RawValue] {
         var rawValues = [RawValue]()
         var raw = 0
